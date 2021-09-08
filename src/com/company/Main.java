ec57f025359ebef1668e8759063c239bb1bd31d9
Rezendes <LREZENDES@student.bridgew.edu>
2021-09-08 14:29:01 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    var reader = new Scanner(System.in);
+	    System.out.print("How old are you: ");
+	    var age Scanner = reader.nextfloat();
+	    var dogYears = age*7;
+	    System.out.println(x: "Wow you are "+ age + " that is "+ dogYears+ " in dog years");
+    }
+}
